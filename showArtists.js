1522b0ce3e9474c98fa838b6d8c29339f308de92
chaseschlachter <45405981+chaseschlachter@users.noreply.github.com>
2021-12-24 10:37:11 -0600

    Update showArtists.js

--- showArtists.js
+++ showArtists.js
@@ -1,7 +1,5 @@
 // Artist's page where I'd like show guestsAttend for each individual event
 
-<h2>Events</h2>
-
 <% for (let events of artist.events ) {%>
 
   <div class="card mb-3 shadow">
@@ -10,10 +8,7 @@
               <div class="card-body">
                   <h5 class="card-title"><%= events.event_name %> </h5>
                   <p class="text"><%= events.description %> </p>
-                  <p class="text"><%= events.eventData %> </p>
-                                       
-  // row where I'd like to display the count for each individual event for a given artist 
-                  <p class="text">Guests signed up <%= guestAttends %> </p>
+                  <p class="text"><%= events.totalGuests %> </p>
                   <p class="text">
                       <small class="text-muted"><%= events.description %> </small>
                   </p>
@@ -23,5 +18,4 @@
       </div>
   </div>
 <% }%>
-
  
