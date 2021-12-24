0d3c8a8ab48c80b065ede6f1e1bfb5297dea4763
chaseschlachter <45405981+chaseschlachter@users.noreply.github.com>
2021-12-23 21:48:48 -0600

    Create event.js
    mongodb model for events

---
+++ event.js
@@ -0,0 +1,42 @@
+// my database model
+
+const eventSchema = new Schema({
+  event_name: String,
+  venue_name: String,
+  address: String,
+  description: String,
+  image: String,
+  geometry: {
+    type: {
+        type: String,
+        enum: ['Point'],
+        required: true
+    },
+    coordinates: {
+        type: [Number],
+        required: true
+    }
+  },
+  artist: {
+    type: Schema.Types.ObjectId,
+    ref: 'Artist'
+  },
+  guests: [
+    {
+      email: String,
+      attended: String // this will be the "Y" or blank
+    }
+  ],
+  created: {
+    type: Date,  // Captures both date and time
+    default: Date.now
+  },
+  event_start: {
+    type: Date,
+    required: [true, 'Date & time of event start required']
+    },
+  event_end: {
+    type: Date,
+    required: [true, 'Date & time of event end required']
+  },
+}, opts);
